6d29403678fef3d496373b5f5f31ca962726242a
3dpuzzlemarketing-hash <3dpuzzlemarketing@gmail.com>
2026-02-17 19:02:57 -0700

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'REPLACE_ME',
-  authDomain: 'REPLACE_ME',
-  projectId: 'REPLACE_ME',
-  storageBucket: 'REPLACE_ME',
-  messagingSenderId: 'REPLACE_ME',
-  appId: 'REPLACE_ME'
+  apiKey: "AIzaSyCig388eT5X2EqQqkz_TAAf9LBp-021yNg",
+  authDomain: "event-pos-7b94d.firebaseapp.com",
+  projectId: "event-pos-7b94d",
+  storageBucket: "event-pos-7b94d.firebasestorage.app",
+  messagingSenderId: "852901304417",
+  appId: "1:852901304417:web:4c57f2b093ac9946a43220",
+  measurementId: "G-NKREDNMEKP"
 };
